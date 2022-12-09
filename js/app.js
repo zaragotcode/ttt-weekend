3822fb78f1281fc7448432986d5107b94e08df34
Patrick Mazara <patrickmazara@hotmail.com>
2022-12-08 19:47:57 -0500

    add winningCombo variable

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const winningCombos = [
+    // Horizonal Combos
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // Vertical Combos
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // Diagonal Combos
+    [0, 4, 8],
+    [2, 4, 6],
+]
 
 
 /*---------------------------- Variables (state) ----------------------------*/
@@ -68,6 +80,8 @@ function updateMessage() {
         messageEl.textContent = `${turn} has won!`
     }
 } 
+
+
 
 
 
