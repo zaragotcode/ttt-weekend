2f4022ae36d25d56e7d79a9e6f706f45072e07ce
Patrick Mazara <patrickmazara@hotmail.com>
2022-12-08 19:01:43 -0500

    complete init function and updateBoard function

--- js/app.js
+++ js/app.js
@@ -3,11 +3,25 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
+// (BOARD) - represent state of the squares on the board
+let board = [null, null, null, null, null, null, null, null, null,]
+
+// (TURN) - track player turn
+let turn = 1
+
+// (WINNER) - represent if anyone has one yet
+let winner = false
+
+// (TIE) - represend if game has ended in tie
+let tie = false
 
 
 
 /*------------------------ Cached Element References ------------------------*/
 
+const squareEls = document.querySelectorAll(".sqr")
+
+const messageEl = document.getElementById("#message")
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -16,3 +30,32 @@
 
 /*-------------------------------- Functions --------------------------------*/
 
+function init () {
+    // board = [null, null, null, null, null, null, null, null, null,]
+    // turn = 1
+    // winner = false
+    // tie = false
+    render()
+}
+
+init()
+
+function render() {
+
+}
+
+function updateBoard() {
+    board.forEach(function(square, idx) { 
+        if(square === 1) {
+            squareEls[idx].textContent = "X"
+        } else if (square === -1) {
+            squareEls[idx].textContent = "O"
+        } else {
+            squareEls[idx].textContent = ""
+        }
+    })
+}
+
+updateBoard()
+
+console.log(updateBoard)
